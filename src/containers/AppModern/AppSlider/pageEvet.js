3b723f53154a2c20b9c59944154054b90233c3b5
amohammdi <alif.mohamady20@gmail.com>
2021-04-09 11:33:55 +0430

    update v-4

--- src/containers/AppModern/AppSlider/pageEvet.js
+++ src/containers/AppModern/AppSlider/pageEvet.js
@@ -26,7 +26,7 @@ const AppSlider = () => {
   return (
     <SectionWrapper>
       <Container>
-        <CarouseWrapper style={{ boxShadow: "0 50 66" }}>
+        <CarouseWrapper>
           <GlideCarousel
             bullets={true}
             controls={false}
